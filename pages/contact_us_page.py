bcc57f1c0c7764cffd7dd3b841538529b33e9d44
Nikolay Makarchuk <makarchuk93@gmail.com>
2025-11-19 14:46:05 +0300

    added locators

--- pages/contact_us_page.py
+++ pages/contact_us_page.py
@@ -1,7 +1,8 @@
 from pages.base_page import BasePage
+from pages.locators import contact_us_locators as locators
 
 
 class ContactUsPage(BasePage):
     page_url = '/contactus'
 
-    
+    def contact_us_form_send(self, name, phone, email, company, subject, question):
---
+++ pages/locators/contact_us_locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+NAME_FIELD = (By.ID, 'contact1')
+PHONE_FIELD = (By. ID, 'contact2')
+EMAIL_FIELD = (By. ID, 'contact3')
+COMPANY_FIELD = (By. ID, 'contact4')
+SUBJECT_FIELD = (By. ID, 'contact5')
+QUESTION_FIELD = (By. ID, 'contact6')
+SEND_BUTTON = (By. XPATH, '//a[contains(text(), "Send")]')
