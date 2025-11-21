1cd5f97f49b39b73eb5faf04d3e6743f2e1afec2
Depfloyd <makarchuk93@gmail.com>
2025-11-21 17:53:20 +0300

    added pytest.marks

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -7,11 +7,13 @@ def test_page_title(cart_page):
     cart_page.check_page_title('Order overview')
 
 
+@pytest.mark.usefixtures('cart_page')
 def test_empty_cart_message(cart_page):
     cart_page.open_page()
     cart_page.check_empty_cart_message('Your cart is empty!')
 
 
+@pytest.mark.usefixtures('cart_page')
 def test_payment_bread_crumbs(cart_page):
     cart_page.open_page()
     cart_page.check_payment_bread_crumbs('Payment')
