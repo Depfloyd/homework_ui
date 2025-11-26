a64df425c0a97b8f45334ba2d77c85d1ef88e9e5
Nikolay Makarchuk <makarchuk93@gmail.com>
2025-11-26 17:58:41 +0300

    added shop locators

---
+++ pages/locators/shop_locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+CUSTOMIZABLE_DESK_TITLE = (
+    By.XPATH, '//a[@class="text-primary text-decoration-none" and contains(text(), "Customizable Desk")]'
+    )
+CUSTOMIZABLE_DESK_PRICE = (By.XPATH, '//span[@class="oe_currency_value" and contains(text(), "750.00")]')
+SEARCH_FIELD = (By.XPATH, '(//input[@type="search" and @name="search" and @placeholder="Search..."])[2]')
